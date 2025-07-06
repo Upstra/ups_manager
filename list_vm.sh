31ad37e6dc511f8001f70f9671c6af877f5c7f76
Ronan Kielt <kakapo9156@gmail.com>
2025-07-06 15:37:46 +0200

    fix: bash files takes parameters

--- list_vm.sh
+++ list_vm.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
+# Usage: ./list_vm.sh --ip <IP> --user <USER> --password <PASS> [--port <PORT>]
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --ip) IP="$2"; shift ;;
+        --user) USER="$2"; shift ;;
+        --password) PASSWORD="$2"; shift ;;
+        --port) PORT="$2"; shift ;;
+        *) echo "Unknown parameter: $1" ; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ -z "$IP" || -z "$USER" || -z "$PASSWORD" ]]; then
+    echo "ERROR : Missing parameter"
+    echo "Usage: $0 --ip <IP> --user <USER> --password <PASS> [--port <PORT>]"
+    exit 1
+fi
+
 source .venv/bin/activate
-python list_vm.py
+
+CMD="python list_vm.py --ip \"$IP\" --user \"$USER\" --password \"$PASSWORD\""
+if [[ -n "$PORT" ]]; then
+    CMD+=" --port \"$PORT\""
+fi
+eval "$CMD"
