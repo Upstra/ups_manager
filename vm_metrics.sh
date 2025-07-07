a93dc0c4f5f5420530611c3286b88054d6b1b687
Ronan Kielt <kakapo9156@gmail.com>
2025-07-08 01:40:58 +0200

    fix: update bash script for uuid argument

--- vm_metrics.sh
+++ vm_metrics.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Usage: ./vm_metrics.sh --vm <VM> --datacenter <DATACENTER> --ip <IP> --user <USER> --password <PASS> [--port <PORT>]
+# Usage: ./vm_metrics.sh --uuid <UUID> --ip <IP> --user <USER> --password <PASS> [--port <PORT>]
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --vm) VM="$2"; shift ;;
-        --datacenter) DATACENTER="$2"; shift ;;
+        --uuid) UUID="$2"; shift ;;
         --ip) IP="$2"; shift ;;
         --user) USER="$2"; shift ;;
         --password) PASSWORD="$2"; shift ;;
@@ -15,9 +14,9 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-if [[ -z "$VM" || -z "$DATACENTER" || -z "$IP" || -z "$USER" || -z "$PASSWORD" ]]; then
+if [[ -z "$UUID" || -z "$IP" || -z "$USER" || -z "$PASSWORD" ]]; then
     echo "ERROR : Missing parameter"
-    echo "Usage: $0 --vm <VM> --datacenter <DATACENTER> --ip <IP> --user <USER> --password <PASS> [--port <PORT>]"
+    echo "Usage: $0 --uuid <UUID> --ip <IP> --user <USER> --password <PASS> [--port <PORT>]"
     exit 1
 fi
 
@@ -30,7 +29,7 @@ if ! source .venv/bin/activate; then
     exit 1
 fi
 
-CMD="python vm_metrics.py --vm \"$VM\" --datacenter \"$DATACENTER\" --ip \"$IP\" --user \"$USER\" --password \"$PASSWORD\""
+CMD="python vm_metrics.py --uuid \"$UUID\" --ip \"$IP\" --user \"$USER\" --password \"$PASSWORD\""
 if [[ -n "$PORT" ]]; then
     CMD+=" --port \"$PORT\""
 fi
