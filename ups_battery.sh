1fbacc4579fe5f5c4316b131417335d54743c2dc
Ronan Kielt <kakapo9156@gmail.com>
2025-07-08 15:54:06 +0200

    feat: create script to get ups minutes left

---
+++ ups_battery.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: ./ups_battery.sh --ip <IP>
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --ip) IP="$2"; shift ;;
+        *) echo "Unknown parameter: $1" ; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ -z "$IP" ]]; then
+    echo "ERROR : Missing parameter"
+    echo "Usage: $0 --ip <IP>"
+    exit 1
+fi
+
+CMD="/bin/snmpget -OT -mall -c\"public\" -v1 \"$IP\" UPS-MIB::upsEstimatedMinutesRemaining.0 | /bin/awk '{print$4}'"
+eval "$CMD"
