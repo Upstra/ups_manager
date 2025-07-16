5c83d330e87c2890adc530bcece3e82cc066c6f0
Ronan Kielt <kakapo9156@gmail.com>
2025-07-16 02:48:54 +0200

    feat: multiple exceptions catched in vm_start

--- vm_start.py
+++ vm_start.py
@@ -24,7 +24,7 @@ def vm_start(vm: vim.VirtualMachine, name: str) -> dict:
         task = vm.PowerOn()
         WaitForTask(task)
         return result_message(f"VM '{name}' has been successfully started", 200)
-    except vim.fault.NoCompatibleHost as _:
+    except (vim.fault.NoCompatibleHost, vim.fault.InvalidHostState, vim.fault.HostNotConnected) as _:
         return result_message("Host is unreachable", 404)
     except Exception as err:
         return result_message(str(err), 400)
